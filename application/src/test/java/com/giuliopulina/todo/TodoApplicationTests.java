918180832410d4fe38cf9e9ea827e9599118c853
Giulio Pulina <giuliopulina@users.noreply.github.com>
2022-12-11 23:09:36 +0100

    Hack to make test working on CI

--- application/src/test/java/com/giuliopulina/todo/TodoApplicationTests.java
+++ application/src/test/java/com/giuliopulina/todo/TodoApplicationTests.java
@@ -1,10 +1,21 @@
 package com.giuliopulina.todo;
 
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
 
 @SpringBootTest
 class TodoApplicationTests {
+
+	@BeforeAll
+	public void setup() {
+		/* FIXME: hack to make test working on CI */
+		System.setProperty("aws.region", "eu-west-2");
+	}
 
 	@Test
 	void contextLoads() {
