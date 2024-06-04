dd9865aa78eafb9aafb0f04bba0a62d63426c309
Giulio Pulina <giuliopulina@users.noreply.github.com>
2024-06-04 15:34:37 +0200

    Fixed integrationTest configuration

--- application/src/test/java/net/giuliopulina/stratospheric/ApplicationContextTest.java
+++ application/src/test/java/net/giuliopulina/stratospheric/ApplicationContextTest.java
@@ -5,8 +5,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.EnabledIf;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
@@ -21,29 +21,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
-//profile that needs to be there to activate the test
-@Profile("integrationTest")
+@EnabledIf(expression = "#{systemProperties['integrationTest'] != null}", loadContext = false)
 //profile that will be set to the SpringBoot application that is going to be integration tested
 @ActiveProfiles("local")
 public class ApplicationContextTest {
 
-    @Container
-    public static DockerComposeContainer<?> environment =
-            new DockerComposeContainer<>(new File("docker-compose.yml"))
-                    .withExposedService("postgres_1", 5432, Wait.forListeningPort())
-                    .withExposedService("localstack_1", 4566)
-                    .withExposedService("keycloak_1", 8080,
-                            Wait.forHttp("/").forStatusCode(200)
-                                    .withStartupTimeout(Duration.ofSeconds(45)))
-                    .withOptions("--compatibility")
-                    .withLocalCompose(true);
-
-    static {
-        environment.start();
-    }
-
     @Autowired
     private ApplicationContext context;
+
+    @Container
+    public static DockerComposeContainer<?> dockerComposeEnvironment = new DockerComposeContainer<>(
+            new File("docker-compose.yml"))
+            .withExposedService("postgres_1", 5432, Wait.forListeningPort())
+            .withExposedService("localstack_1", 4566)
+            .withExposedService("keycloak_1", 8080, Wait.forHttp("/")
+                    .forStatusCode(200).withStartupTimeout(Duration.ofSeconds(45)))
+            .withOptions("--compatibility")
+            .withLocalCompose(true);
+
+    static {
+        dockerComposeEnvironment.start();
+    }
 
     @Test
     public void contextLoads() {
